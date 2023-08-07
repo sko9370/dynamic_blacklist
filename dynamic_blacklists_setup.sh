400a1f2abae46acb556b59e8823f3aa0fc01f575
Sang Oh <28380215+sko9370@users.noreply.github.com>
2023-08-07 09:44:47 -0400

    Add files via upload

--- dynamic_blacklists_setup.sh
+++ dynamic_blacklists_setup.sh
@@ -2,9 +2,10 @@
 
 mkdir /config/dynamic_blacklists
 touch /config/dynamic_blacklists/LocalWhitelist.txt /config/dynamic_blacklists/LocalBlacklist.txt
+rm /config/dynamic_blacklists/update_blacklist.sh
+curl https://raw.githubusercontent.com/sko9370/dynamic_blacklist/main/update_blacklist.sh > /config/dynamic_blacklists/update_blacklist.sh
 
-#cronjob="0 * * * * bash update_blacklist.sh"
-cronjob="* * * * * bash /config/dynamic_blacklists/update_blacklist.sh"
+cronjob="0 * * * * /bin/bash /config/dynamic_blacklists/update_blacklist.sh"
 
 # https://stackoverflow.com/questions/878600/how-to-create-a-cron-job-using-bash-automatically-without-the-interactive-editor
 ( crontab -l | grep -v -F "blacklist"; echo "$cronjob" ) | crontab -
